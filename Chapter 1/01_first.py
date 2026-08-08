d5893ab478f2dc2f162d1c692fe92d0faf343ca6
tasbeeh479 <tasbeehullah2007@gmail.com>
2026-08-08 16:19:06 +0500

    Variables and Data types

---
+++ Chapter 1/01_first.py
@@ -0,0 +1,20 @@
+print ("How are You")
+# I M FINE                      (single line comment)
+"""WHAT ABOUT YOU?              (multple line comment)
+SAY SMTH ABOUT YOURSELF."""
+'''I AM PRETTY FINE'''
+print("""tell me smth about yourself        
+      what's ur name?""")
+# Also for multiple line print
+
+Name = "Tasbeeh"
+print("Good Afternoon!",Name)
+# OR
+print("Good Afternoon! " + Name)           # String Concatination
+# OR
+print(f"Good Afternoon! {Name}")           # "f" means; Print things written in {} firstly
+
+
+# print() is a built in function use to print something 
+
+
---
+++ Chapter 1/02_Variables&Datatypes.py
@@ -0,0 +1,31 @@
+# Arithematic OPerators:-
+a=23
+b=45
+c=a+b
+print(c)
+
+#Assignment Operators:-
+d=b
+print(d)
+d+=5        #d-=5   d*=5    d/=5
+print(d)
+
+#Comparison Operators:-         (Always give answer in True or False)
+z= d==b
+print(z)
+e= 6==6
+print(e)
+f= d>=b
+print(f)
+g= d<=b
+print(g)
+
+#Logical Operators:-
+h=False
+i=True
+j=None          #no value
+
+# AND OR NOT etc
+
+
+
