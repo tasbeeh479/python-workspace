5a5b15b96dd0d0b09c7c93cdf249be554d0f9b7a
tasbeeh479 <tasbeehullah2007@gmail.com>
2026-08-11 07:58:26 +0500

    Tuples Definitions

---
+++ Chapter 4/03_Tuple.py
@@ -0,0 +1,15 @@
+# Tuple:- List but immmutable (use'()' instesd'[]')
+
+a = (9,2,3,4,5,6)
+print(type(a))
+
+b = (1)                 # Considered as 'int'
+print(type(b))
+
+c = (5,)                # Right way to write typle having singe element
+print(type(c))
+
+a = (23,34.5,"tasbeeh",True,"Rohan",2343423.234)
+print(a)
+
+# a[0] = "abcd"           Error:- IMMUTABLE
